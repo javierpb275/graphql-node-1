0b7cc8d74cc05189eecf891083b0c67acb43f518
javierpb275 <javierpb275@gmail.com>
2024-02-18 18:43:48 +0100

    creating types

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -3,9 +3,31 @@ import { createSchema } from "graphql-yoga";
 
 export const schema = createSchema({
   typeDefs: gql`
+    type Company {
+      name: String
+      catchPhrase: String
+      bs: String
+    }
+    type Geo {
+      lat: String
+      lng: String
+    }
+    type Address {
+      street: String
+      suite: String
+      city: String
+      zipcode: String
+      geo: Geo
+    }
     type User {
       id: Int
       name: String
+      username: String
+      email: String
+      phone: String
+      website: String
+      company: Company
+      address: Address
     }
     type Query {
       hello: String
