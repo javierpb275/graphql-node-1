770a84496b9955ccb7274ad2cd99e6831c4931cd
javierpb275 <javierpb275@gmail.com>
2024-02-18 18:11:53 +0100

    adding opbject type and creating resolver for it

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -3,15 +3,21 @@ import { createSchema } from "graphql-yoga";
 
 export const schema = createSchema({
   typeDefs: gql`
+    type User {
+      id: Int
+      name: String
+    }
     type Query {
       hello: String
       number: Int
+      user: User
     }
   `,
   resolvers: {
     Query: {
       hello: () => "world",
       number: () => 1,
+      user: () => ({ id: 1, name: "Pepe" }),
     },
   },
 });
