7de84765ee38d11e062b019eaff264a47713f8ae
javierpb275 <javierpb275@gmail.com>
2024-03-01 13:15:44 +0100

    added ws server, apollo server, started apollo server, applied middlewares and strated http server

--- server.ts
+++ server.ts
@@ -1,7 +1,15 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import bodyParser from "body-parser";
+import cors from "cors";
 import express from "express";
 import { PubSub } from "graphql-subscriptions";
 import gql from "graphql-tag";
+import { useServer } from "graphql-ws/lib/use/ws";
 import { createServer } from "http";
+import { WebSocketServer } from "ws";
 
 //Asynchronous Anonymous Function
 // Inside of server.ts -> await keyword
@@ -58,4 +66,47 @@ import { createServer } from "http";
       },
     },
   };
+
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+  // ws Server
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: "/graphql", // localhost:3000/graphql
+  });
+
+  const serverCleanup = useServer({ schema }, wsServer); // dispose
+
+  // apollo server
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
+  });
+
+  // start our server
+  await server.start();
+
+  // apply middlewares
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    bodyParser.json(),
+    expressMiddleware(server)
+  );
+
+  // http server start
+  httpServer.listen(4000, () => {
+    console.log("Server running on http://localhost:" + "4000" + "/graphql");
+  });
 })();
